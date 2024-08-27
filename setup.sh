8d57256121754035bc4eb9b4dad872438bf6212a
Karan Bhateja <40824205+Karanbhateja@users.noreply.github.com>
2024-08-27 16:16:01 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,7 +9,8 @@ cd xmrig-6.22.0
 # Setup configuration (make sure you replace this with your actual configuration)
 # You should create a config.json file or adjust this script as needed
 
-wget https://raw.githubusercontent.com/karanbhateja/mine/main/config.json
+# wget https://raw.githubusercontent.com/karanbhateja/mine/main/config.json
 
 # Start mining
+./xmrig -o pool.supportxmr.com:443 -u 47qUtr22oGDgGsYbfUBKfpbyRa5NqTCcR9m4bP2w3jiD3XC1RZ2bKTiVLaqi9wcP4FB8he1XT461J6aPjH6gxw7cU7GQsh3 -k --tls
 # ./xmrig
