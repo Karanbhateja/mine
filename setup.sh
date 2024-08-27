1df2f65b795a0947fed9718d69776f9f33f13b80
Karan Bhateja <40824205+Karanbhateja@users.noreply.github.com>
2024-08-27 14:23:45 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ sudo apt install -y build-essential cmake libuv1-dev libssl-dev wget tar
 
 wget https://github.com/xmrig/xmrig/releases/download/v6.22.0/xmrig-6.22.0-jammy-x64.tar.gz
 tar -xzf xmrig-6.22.0-jammy-x64.tar.gz
-cd xmrig-6.22.0-jammy-x64
+cd xmrig-6.22.0
 
 # Setup configuration (make sure you replace this with your actual configuration)
 # You should create a config.json file or adjust this script as needed
